60f15a1fc3276a582760f6308b4033a18c0d8fb9
pstrein <pstrein@epic.com>
2021-09-27 14:30:19 -0500

    Add boilerplate blog

---
+++ src/utils/Config.ts
@@ -0,0 +1,9 @@
+export const Config = {
+  site_name: 'Peter Strein Blog',
+  title: 'Peter Strein Blog',
+  description: 'The adventures of Peter Strein in the scary world of blogging',
+  url: 'https://example.com',
+  locale: 'en',
+  author: 'Peter Strein',
+  pagination_size: 10,
+};
