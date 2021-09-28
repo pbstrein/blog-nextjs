216ddda2598c4cbb70e40e1e9755849706e7d9c6
pstrein <pstrein@epic.com>
2021-09-28 09:32:56 -0500

    Change title to purple

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -17,7 +17,7 @@ const Main = (props: IMainProps) => (
     <div className="max-w-screen-md mx-auto">
       <div className="border-b border-gray-300">
         <div className="pt-16 pb-8">
-          <div className="font-semibold text-3xl text-gray-900">{Config.title}</div>
+          <div className="font-semibold text-3xl text-purple-500">{Config.title}</div>
           <div className="text-xl">{Config.description}</div>
         </div>
         <div>
